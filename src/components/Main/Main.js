4f8b39c815d05cd2ae7f22ba1c3fc56af771ceab
Neele Wähnke <neele.waehnke@hotmail.de>
2022-11-21 09:43:28 +0100

    update structure of components and add more styling

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,47 +1,12 @@
 import './Main.css';
+import Entryform from './Entry-form/Entryform';
+import Entries from './Entries/Entries';
 
 export default function Main() {
   return (
     <main className="main">
-      <form action="" className="form">
-        <label htmlFor="input__text" className="label">
-          Motto
-        </label>
-        <input
-          type="text"
-          id="input__text"
-          name="input__text"
-          className="input__text"
-        />
-        <label htmlFor="input__textarea" className="label">
-          Notes
-        </label>
-        <textarea
-          name="input__textarea"
-          className="input__textarea"
-          id="input__textarea"
-          cols="30"
-          rows="10"
-        ></textarea>
-        <button type="submit" id="form__button" className="form__button">
-          Create
-        </button>
-      </form>
-      <div className="tabs">
-        <button>
-          All Entries
-          <span className="tab__badge">3</span>
-        </button>
-        <button>
-          Favorites
-          <span className="tab__badge">1</span>
-        </button>
-      </div>
-      <section className="entries">
-        <h5>date</h5>
-        <h4>Title</h4>
-        <p>Notes</p>
-      </section>
+      <Entryform />
+      <Entries />
     </main>
   );
 }
