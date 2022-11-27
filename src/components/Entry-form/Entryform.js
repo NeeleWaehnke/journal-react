4d6bbe0a8ccc732721560fd0bae3b90adeea1fb6
Neele Wähnke <neele.waehnke@hotmail.de>
2022-11-27 15:40:57 +0100

    add function to form

--- src/components/Entry-form/Entryform.js
+++ src/components/Entry-form/Entryform.js
@@ -1,25 +1,32 @@
 import './Entryform.css';
 
-export default function Entryform() {
+export default function Entryform({ onAddEntry }) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = event.target;
+    const formElements = form.elements;
+    const data = {
+      motto: formElements.motto.value,
+      notes: formElements.notes.value,
+    };
+    onAddEntry(data);
+    event.target.reset();
+  }
+
   return (
     <>
-      <h2 className="entryform__title">New Entry</h2>
-      <form action="" className="form">
+      <form action="" className="form" onSubmit={handleSubmit}>
+        <h2 className="entryform__title">New Entry</h2>
         <label htmlFor="input__text" className="label">
           Motto
         </label>
-        <input
-          type="text"
-          id="input__text"
-          name="input__text"
-          className="input__text"
-        />
+        <input type="text" id="motto" name="motto" className="input__text" />
         <label htmlFor="input__textarea" className="label">
           Notes
         </label>
         <textarea
-          name="input__textarea"
-          id="input__textarea"
+          name="notes"
+          id="notes"
           className="input__textarea"
           cols="30"
           rows="10"
